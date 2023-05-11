7f77bcfc8635ad26a66ed81eda2197f41c116189
ndasteven <ndasteven5@gmail.com>
2023-05-11 11:03:18 +0000

    new

--- admin/vue.php
+++ admin/vue.php
@@ -17,18 +17,30 @@ require 'db_connection.php';
     //requete pour selectionner toute les Utilisateurs
     $sql = "SELECT *, Gare.nom_Gare, COUNT(Ticket.id_ticket) as nbre_ticket, SUM(Ticket.prix_Ticket) as total_ticket
             FROM Utilisateurs LEFT JOIN Gare ON Utilisateurs.id_gare = Gare.id_gare
-             LEFT JOIN Ticket ON Utilisateurs.id_utilisateurs = Ticket.id_utilisateurs GROUP BY Utilisateurs.id_utilisateurs
-             ORDER BY  total_ticket DESC";
+            LEFT JOIN Ticket ON Utilisateurs.id_utilisateurs = Ticket.id_utilisateurs GROUP BY Utilisateurs.id_utilisateurs
+            ORDER BY  total_ticket DESC";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $USERS = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //requete pour selectionner le nombre ticket vendu appenant a une gare
+
+    $sql = "SELECT Gare.nom_Gare, COUNT(Ticket.id_ticket) as nbre_ticket
+            FROM Gare 
+            LEFT JOIN Utilisateurs ON Gare.id_gare = Utilisateurs.id_gare
+            LEFT JOIN Ticket ON Ticket.id_utilisateurs = Utilisateurs.id_utilisateurs 
+            GROUP BY Gare.nom_Gare ORDER BY nbre_ticket DESC;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $NBRE_TICKET_GARE = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
     $json_data = array(
     'gare'=>$GARES,
     'user'=>$USERS,
-    'TOTAL_PRIX_TICKET'=>$montant_total_ticket
+    'TOTAL_PRIX_TICKET'=>$montant_total_ticket,
+    'nbre_ticket_gare'=>$NBRE_TICKET_GARE
     );
         
     
